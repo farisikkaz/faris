9a9d9ea017223147efc4d682e0718e164e26f189
farisikkaz <dulquerjr10@gmail.com>
2023-09-19 16:51:33 +0530

    initial commit

---
+++ bin/variables/cellphone.dart
@@ -0,0 +1,27 @@
+class Cellphones {
+  String? name;
+  int? model;
+  int? ram;
+  String? battery;
+  int? rom;
+}
+void main() {
+  Cellphones c1 = Cellphones();
+
+  print("cellphone 1 name =${c1.name = 'vivo'}");
+  print("cellphone 1 model =${c1.model = 05}");
+  print("cellphone 1 ram =${c1.ram = 8}");
+  print("cellphone 1 battery =${c1.battery = '4000mah'}");
+  print("cellphone 1 rom = ${c1.rom = 128}");
+
+  print(object)
+  Cellphones c2 = Cellphones();
+
+  print("cellphone 2 name =${c2.name = 'realme'}");
+  print("cellphone 2 model =${c2.model = 05}");
+  print("cellphone 2 ram =${c2.ram = 8}");
+  print("cellphone 2 battery =${c2.battery = '4000mah'}");
+  print("cellphone 2 rom = ${c2.rom = 128}");
+}
+  
+}
