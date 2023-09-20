865703161c7fa3b72a768ffbc54851f3d277a012
farisikkaz <dulquerjr10@gmail.com>
2023-09-20 13:04:57 +0530

    initial commit

--- bin/variables/local_variable2.dart
+++ bin/variables/local_variable2.dart
@@ -1,14 +1,15 @@
-class MyClass{
+class MyClass {
   int c = 1000;
 
-  void addition(){
-  int a =1900,
-  b = 2000;
-  print('SUM =${a+b+c}');
+  void addition() {
+    int a = 1900,
+        b = 2000;
+    print('SUM =${a + b}');
+  }
 }
 
  void main (){
-  MyClass obj= MyClass();
+  MyClass obj = MyClass();
   obj.addition();
 }
 
