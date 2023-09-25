606bf998f0becdf95027218ec792a9f910691799
farisikkaz <dulquerjr10@gmail.com>
2023-09-25 15:04:55 +0530

    initial commit

---
+++ bin/programmes/sum_even_odd.dart
@@ -0,0 +1,17 @@
+void main () {
+  var sum = 0;
+  for (int e = 1; e < 10; e++) {
+    if (e % 2 == 0) {
+      sum += e;
+    }
+  }
+  print(sum);
+print("--------------");
+  var add = 0;
+  for (int o = 1; o < 10; o++) {
+    if (o % 2 == 1) {
+      add += o;
+    }
+  }
+  print(add);
+}
