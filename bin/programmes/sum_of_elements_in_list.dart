dc92ac4e65d4cfbe136f16085a3f43bf85fb7f12
farisikkaz <dulquerjr10@gmail.com>
2023-09-27 13:19:03 +0530

    initial commit

---
+++ bin/programmes/sum_of_elements_in_list.dart
@@ -0,0 +1,27 @@
+void main() {
+  List<int> list = [1, -3, 7, 9, 0, -6, 4, -2, 0, 10, -8, 5];
+  var sum = 0;
+  var sum1=0;
+  for (var i = 0; i < list.length; i++) {
+    sum += list[i];
+  }
+  print(sum);
+  {
+    for(var i=0;i<list.length;i++){
+      if(i%2==0){
+        sum1 += list[i];
+        }
+      }
+    print(sum1);{
+      var largestlist = list[0];
+
+      for (var i = 0; i>largestlist.bitLength; i++){
+        if(list[i]>largestlist){largestlist = list[i];
+
+      }
+        print('largest value in list': $largestlist);
+    }
+
+    }
+    }
+
--- bin/variables/list_set_example.dart
+++ bin/variables/list_set_example.dart
@@ -21,7 +21,7 @@ void main(){
   print(i);
  }
  ///SET
- var set1 ={};
+ var set1 ={1,3,5,7,9};
  var set2 = {1,2,3,4,5,6,7,8,9,10,}; //dynamic set
  Set<int> set3 ={10,30,50,70,90,110};
  set3.add(13);
@@ -30,5 +30,30 @@ void main(){
  for(int index = 0; index<set3.length; index ++){
   print(set3.elementAt(index));
  }
+print(set2.union(set3));        // combine both set
+ print(set1.intersection(set2)); // combine value in both set
+ print(set2.difference(set1));   // value present in set2 and not present in set1
+
+ /// map
+ /// - store value as key-value pairs keys must be unique values can be same or different
+ var map1 = {'name' :'faris', 'age' : 21, 'phone' : 9072361282}; //Map <String,dynamic> map 1
+ // Map<int,int> map2 ={1:90, 2 : 100};
+ map1.addAll({'email': 'farisikz14@gmail.com'});
+ map1['pincode']=690514;
+ print(map1);
+ map1['age ']=22;
+ print(map1);
+ map1.forEach((key, value) {print('$key:$value');
+ });
+ print(map1.containsKey('hello'));
+ print(map1.containsValue(22));
+
+
+
+
+
+
+
+
 
 }
